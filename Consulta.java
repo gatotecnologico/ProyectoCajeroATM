267a2999071d903d77ded423cfce0b447bcc6608
gatotecnologico <L20170684@culiacan.tecnm.mx>
2025-05-27 19:34:53 -0700

    Proyecto

---
+++ Consulta.java
@@ -0,0 +1,13 @@
+
+public class Consulta extends Clase_Abstractaa{
+    
+    @Override
+    public void Transacciones(){
+        System.out.println("====================================");
+        System.out.println("=================BBVA===============");
+        System.out.println("				Debito				");
+        System.out.println("Se relalizo una Consulta el 31/10/2023");
+        System.out.println("Tu saldo actual es: " + getSaldo());
+        System.out.println("====================================");
+    }    
+}
